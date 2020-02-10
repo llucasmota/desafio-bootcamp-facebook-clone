fa89d0314d0cd55965aed7955e5cf523c7f1de70
llucasmota <lucas.o.mota@gmail.com>
2020-02-10 19:41:59 -0300

    Ajustes de estrutura do projeto e configuração do prettier, eslint e editorconfig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import "./App.css";
-import TechList from "./components/TechList";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return <TechList />;
+  return <h1>Hello</h1>;
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <header>dasdasda</header>;
+}
+
+export default Header;
