5abf39c06914fd1951ef26a4efdc24e17dd5ae04
llucasmota <lucas.o.mota@gmail.com>
2020-02-17 13:03:24 -0300

    Alteração e criação da classe do componente principal do body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import PostBody from './components/PostBody';
 import './App.css';
+import Post from './components/PostBody';
 
 function App() {
   return (
-    <section className="grid">
-      <Header />
-    </section>
+    <>
+      <section className="grid">
+        <Header />
+      </section>
+      <PostBody />
+    </>
   );
 }
 
